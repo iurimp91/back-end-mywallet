9038408223fc154891d8bd07e58b8f4c8a9654ca
Iuri Magnago <iurimp91@gmail.com>
2021-06-23 18:52:53 -0300

    Sign Up and Sign In routes added

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import pg from "pg";
+import bcrypt from 'bcrypt';
+import joi from "joi";
+import { v4 as uuid } from "uuid";
 
 const server = express();
 server.use(cors());
@@ -17,11 +20,90 @@ const connection = new Pool({
 });
 
 server.post("/sign-up", async (req, res) => {
+    const userSchema = joi.object({
+        name: joi.string().min(3).max(30).trim().required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(3).max(30).trim().required(),
+    });
+
     try {
+        const userValidation = await userSchema.validateAsync(req.body); 
+        
         const { name, email, password } = req.body;
+
+        const emailValidation = await connection.query(`
+            SELECT * FROM users
+            WHERE email = $1
+        `, [email]);
+
+        if(emailValidation.rows.length !== 0) return res.sendStatus(401);
+
+        const passwordHash = bcrypt.hashSync(password, 12);
+
+        await connection.query(`
+            INSERT INTO users
+            (name, email, password)
+            VALUES ($1, $2, $3)
+        `,[name, email, passwordHash]);
+
+        return res.sendStatus(201);
     } catch(err) {
-        console.log(err);
-        res.sendStatus(500);
+        if(
+            err.message.includes("name")
+            || err.message.includes("email")
+            || err.message.includes("password")
+        ) {
+            console.log(err.message);
+            return res.sendStatus(400);
+        } else {
+            console.log(err.message);
+            return res.sendStatus(500);
+        }
+    }
+});
+
+server.post("/sign-in", async (req, res) => {
+    const userSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().min(3).max(30).trim().required(),
+    });
+
+    try {
+        const userValidation = await userSchema.validateAsync(req.body);
+
+        const { email, password } = req.body;
+
+        const emailValidation = await connection.query(`
+            SELECT * FROM users
+            WHERE email = $1
+        `, [email]);
+
+        const user = emailValidation.rows[0];
+
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = uuid();
+
+            await connection.query(`
+                INSERT INTO sessions
+                ("userId", token)
+                VALUES ($1, $2)
+            `, [user.id, token]);
+
+            return res.send(token);
+        } else {
+            return res.sendStatus(404);
+        }
+    } catch(err) {
+        if(
+            err.message.includes("email")
+            || err.message.includes("password")
+        ) {
+            console.log(err.message);
+            return res.sendStatus(400);
+        } else {
+            console.log(err.message);
+            return res.sendStatus(500);
+        }
     }
 });
 

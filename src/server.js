2e1d0096749044f27535a5af0d0de575bc2c39b1
Iuri Magnago <iurimp91@gmail.com>
2021-08-10 21:43:21 -0300

    refactor: prepare for deploy with Heroku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+import "./setup.js";
 import app from "./app.js";
 
-app.listen(4000, () => {
-    console.log("Server running on port 4000")
+const port = +process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`)
 });
--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,3 +1,5 @@
+import "../src/setup.js";
+
 import supertest from "supertest";
 import app from "../src/app.js";
 import connection from "../src/database.js";
